4a766711a58cb6043a946f73c5e0b23980a06c2e
Stepan Kudin <kudin.stepan@yandex.ru>
2019-07-04 11:20:47 +0300

    Add class for reading script parameters.

--- atlasnet2/scripts/train.py
+++ atlasnet2/scripts/train.py
@@ -1,12 +1,34 @@
 import argparse
-
-import torch
+import json
 
 
 class ParamsReader:
     def __init__(self):
-        pass
+        self._common_params_filename = None
+        self._training_params_filename = None
 
+        self._parser = argparse.ArgumentParser()
+        self._parser.add_argument("-c", "--common_parameters", required=True, type=str,
+                                  help="file with common parameters")
+        self._parser.add_argument("-t", "--training_parameters", required=True, type=str,
+                                  help="file with training parameters")
+
+    def parse_command_prompt(self):
+        args = self._parser.parse_args()
+
+        self._common_params_filename = args.common_parameters
+        self._training_params_filename = args.training_parametes
+
+    def get_common_params(self):
+        return self._parse_params_file(self._common_params_filename)
+
+    def get_training_params(self):
+        return self._parse_params_file(self._training_params_filename)
+
+    @staticmethod
+    def _parse_params_file(filename):
+        with open(filename, "r") as fp:
+            return json.load(fp)
 
 
 class Trainer:
@@ -14,9 +36,11 @@ class Trainer:
 
 
 def main():
-    print("Hello!")
-    test_var = 10
-    print("PyTorch version: %s" % torch.__version__)
+    params_reader = ParamsReader()
+    params_reader.parse_command_prompt()
+
+    common_params = params_reader.get_common_params()
+    training_params = params_reader.get_training_params()
 
 
 if __name__ == "__main__":
