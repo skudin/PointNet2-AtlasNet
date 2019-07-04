e61de837d470913f6d1d7e5bf991fbe2a2547176
Stepan Kudin <kudin.stepan@yandex.ru>
2019-07-04 10:31:29 +0300

    Rename folder.

---
+++ atlasnet2/scripts/train.py
@@ -0,0 +1,23 @@
+import argparse
+
+import torch
+
+
+class ParamsReader:
+    def __init__(self):
+        pass
+
+
+
+class Trainer:
+    pass
+
+
+def main():
+    print("Hello!")
+    test_var = 10
+    print("PyTorch version: %s" % torch.__version__)
+
+
+if __name__ == "__main__":
+    main()
