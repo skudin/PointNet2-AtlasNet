68489d796f30850e56fa3d47c4f6a02b9e5b78c1
Stepan Kudin <kudin.stepan@yandex.ru>
2019-07-12 18:35:25 +0300

    network wrapper: init dataset loaders.

--- atlasnet2/libs/network_wrapper.py
+++ atlasnet2/libs/network_wrapper.py
@@ -2,21 +2,22 @@ from atlasnet2.networks.network import Network
 
 
 class NetworkWrapper:
-    def __init__(self):
-        self._train_data_loader = None
-        self._test_data_loader = None
-        
+    def __init__(self, mode, dataset_path):
+        self._train_data_loader = self._get_data_loader("train", dataset_path, mode)
+        self._test_data_loader = self._get_data_loader("test", dataset_path, mode)
+
         self._network = Network()
 
+        self._loss = None
+
     def train(self):
-        self._train_data_loader = self._get_data_loader("train")
-        self._test_data_loader = self._get_data_loader("test")
+        pass
 
     def test(self):
         pass
 
-    def _get_data_loader(self, dataset_part):
-        pass
+    def _get_data_loader(self, dataset_part, dataset_path, mode):
+        return None
 
     def _init_network(self):
         pass
