239b7b556612d233a4ad2f01cb4cae3d6690fb14
Stepan Kudin <kudin.stepan@yandex.ru>
2019-07-15 17:00:05 +0300

    Init test scripts. Move settings to single file.

--- atlasnet2/scripts/train.py
+++ atlasnet2/scripts/train.py
@@ -1,45 +1,10 @@
-import argparse
-import json
 import logging
 import os
-import shutil
 
 import atlasnet2.libs.helpers as h
+from atlasnet2.libs.settings import Settings
 from atlasnet2.libs.visdom_wrapper import VisdomWrapper
 from atlasnet2.libs.network_wrapper import NetworkWrapper
-
-
-class Settings:
-    def __init__(self):
-        self._common_params_filename = None
-        self._training_params_filename = None
-
-        self._parser = argparse.ArgumentParser()
-        self._parser.add_argument("-c", "--common_parameters", required=True, type=str,
-                                  help="file with common parameters")
-        self._parser.add_argument("-t", "--training_parameters", required=True, type=str,
-                                  help="file with training parameters")
-
-    def parse_command_prompt(self):
-        args = self._parser.parse_args()
-
-        self._common_params_filename = args.common_parameters
-        self._training_params_filename = args.training_parameters
-
-    def get_common_params(self):
-        return self._parse_params_file(self._common_params_filename)
-
-    def get_training_params(self):
-        return self._parse_params_file(self._training_params_filename)
-
-    def save_settings(self, path):
-        shutil.copy(self._common_params_filename, path)
-        shutil.copy(self._training_params_filename, path)
-
-    @staticmethod
-    def _parse_params_file(filename):
-        with open(filename, "r") as fp:
-            return json.load(fp)
 
 
 def main():
