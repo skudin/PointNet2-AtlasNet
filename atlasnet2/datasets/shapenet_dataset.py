fc10deb0cd8450abb0c08f45f9138c437124386b
Stepan Kudin <kudin.stepan@yandex.ru>
2019-07-22 13:52:13 +0300

    Indexing ShapeNet dataset.

--- atlasnet2/datasets/shapenet_dataset.py
+++ atlasnet2/datasets/shapenet_dataset.py
@@ -10,11 +10,20 @@ logger = logging.getLogger(__name__)
 
 
 class ShapeNetDataset(data.Dataset):
-    def __init__(self, dataset_path: str = os.path.join(conf.BASE_PATH, "data", "shapenet")):
+    def __init__(self, dataset_path: str = os.path.join(conf.BASE_PATH, "data", "shapenet"), mode: str = "train"):
         self._dataset_path = dataset_path
+        self._mode = mode
+
+        self._img_path = os.path.join(self._dataset_path, "ShapeNet", "ShapeNetRendering")
+        self._point_clouds_path = os.path.join(self._dataset_path, "customShapeNet")
+
+        self._meta = dict()
 
         self._categories = self._get_categories()
-        logger.debug("Categories: %s" % str(self._categories))
+        logger.info("Categories: %s" % str(self._categories))
+
+        self._indexing()
+        pass
 
     def __len__(self):
         pass
@@ -28,6 +37,39 @@ class ShapeNetDataset(data.Dataset):
         with open(os.path.join(self._dataset_path, "synsetoffset2category.txt"), "r") as fp:
             for line in fp:
                 tokens = line.strip().split()
-                result[tokens[0]] = int(tokens[1])
+                result[tokens[0]] = tokens[1]
 
         return result
+
+    def _indexing(self):
+        empty = []
+        for item in self._categories:
+            img_folder = os.path.join(self._img_path, self._categories[item])
+            img_folder_index = sorted(os.listdir(img_folder))
+
+            ply_folder = os.path.join(self._point_clouds_path, self._categories[item], "ply")
+            # noinspection PyBroadException
+            try:
+                ply_folder_index = sorted(os.listdir(ply_folder))
+            except:
+                ply_folder_index = []
+
+            index = [value for value in img_folder_index if value + ".points.ply" in ply_folder_index]
+            logger.info(
+                "Category: %s, amount of files: %d, %f %%" % (item, len(index), len(index) / len(img_folder_index)))
+
+            if self._mode == "train":
+                index = index[: int(len(index) * 0.8)]
+            else:
+                index = index[int(len(index)) * 0.8:]
+
+            if len(index) != 0:
+                self._meta[item] = list()
+                for filename in index:
+                    self._meta[item].append((os.path.join(img_folder, filename, "rendering"),
+                                             os.path.join(ply_folder, filename + ".points.ply"), item, filename))
+            else:
+                empty.append(item)
+
+        for item in empty:
+            del self._categories[item]
