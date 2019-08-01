9272e18b6dc8d4f5d8e7620829d62b617b622d89
Stepan Kudin <kudin.stepan@yandex.ru>
2019-08-01 20:12:20 +0300

    Small improves.

--- atlasnet2/networks/autoencoder.py
+++ atlasnet2/networks/autoencoder.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+import torch
 import torch.nn as nn
 
 import atlasnet2.networks.atlasnet as atlasnet
@@ -15,12 +18,12 @@ class Autoencoder(nn.Module):
 
         self._decoder = atlasnet.Decoder()
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor):
         x = self._encoder.forward(x)
 
         return self._decoder.forward(x)
 
-    def inference(self, x, num_point=None):
+    def inference(self, x: torch.Tensor, num_point: Union[int, None] = None):
         with torch.no_grad():
             x = self._encoder.forward(x)
 
--- atlasnet2/networks/network.py
+++ atlasnet2/networks/network.py
@@ -5,9 +5,12 @@ from atlasnet2.networks.autoencoder import Autoencoder
 
 
 class Network:
-    def __init__(self, encoder_type: str = "pointnet", learning_rate=0.001):
+    def __init__(self, encoder_type: str = "pointnet", learning_rate: float = 0.001):
         self._network = Autoencoder(encoder_type)
         self._optimizer = optim.Adam(self._network.parameters(), lr=learning_rate)
+
+    def forward(self, x: torch.Tensor):
+        return self._network(x)
 
     def backward(self, loss):
         loss.backward()
