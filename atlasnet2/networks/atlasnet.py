93b7f6def04a01fb118d2c16dc8fcf88509efcd7
Stepan Kudin <kudin.stepan@yandex.ru>
2019-08-01 16:50:06 +0300

    Init encoder/decoder.

---
+++ atlasnet2/networks/atlasnet.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+
+class Encoder(nn.Module):
+    def forward(self, x):
+        pass
+
+
+class Decoder(nn.Module):
+    def forward(self, x):
+        pass
+
+    def inference(self, x, num_points):
+        pass
--- atlasnet2/networks/network.py
+++ atlasnet2/networks/network.py
@@ -1,6 +1,31 @@
-class Network:
-    def forward(self, points_clouds):
-        pass
+import torch
+import torch.nn as nn
+
+import atlasnet2.networks.atlasnet as atlasnet
+import atlasnet2.networks.pointnet2 as pointnet2
+
+
+class Network(nn.Module):
+    def __init__(self, encoder_type: str = "pointnet"):
+        super().__init__()
+
+        if encoder_type == "pointnet":
+            self._encoder = atlasnet.Encoder()
+        else:
+            self._decoder = pointnet2.Encoder()
+
+        self._decoder = atlasnet.Decoder()
+
+    def forward(self, x):
+        x = self._encoder.forward(x)
+
+        return self._decoder.forward(x)
+
+    def inference(self, x, num_point=None):
+        with torch.no_grad():
+            x = self._encoder.forward(x)
+    
+            return self._decoder.inference(x, num_point)
 
     def backward(self, loss):
         pass
