7a60e19f5f2ff6056bafb9d2c52c4bcb93b37718
Stepan Kudin <kudin.stepan@yandex.ru>
2019-08-16 18:30:44 +0300

    setup script prototype.

--- packages/pointnet2_ext/setup.py
+++ packages/pointnet2_ext/setup.py
@@ -1,14 +1,20 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+ext_src_root = "."
+
 setup(
-    name="pointnet2_",
+    name="pointnet2_ext",
     ext_modules=[
-        CUDAExtension('chamfer', [
-            'chamfer_cuda.cpp',
-            'chamfer.cu',
-        ]),
+        CUDAExtension(
+            name="pointnet2_ext",
+            sources=ext_src_root,
+            extra_compile_args={
+                "cxx": ["-O2", "-I{}".format("{}/include".format(ext_src_root))],
+                "nvcc": ["-O2", "-I{}".format("{}/include".format(ext_src_root))],
+            }
+        ),
     ],
     cmdclass={
-        'build_ext': BuildExtension
+        "build_ext": BuildExtension
     })
