dedc02da22b9003c99e92226d3069351d0e9843a
Stepan Kudin <kudin.stepan@yandex.ru>
2019-09-09 00:11:13 +0300

    get dataset statistics: parsing command prompt.

--- atlasnet2/scripts/get_dataset_statistics.py
+++ atlasnet2/scripts/get_dataset_statistics.py
@@ -1,4 +1,18 @@
+import argparse
+
+
+def parse_command_prompt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", required=True, type=str, help="path to npy dataset")
+    parser.add_argument("-o", "--output", required=True, type=str, help="path to writing result")
+
+    args = parser.parse_args()
+
+    return args.input, args.output
+
+
 def main():
+    dataset_path, output_path = parse_command_prompt()
     print("Hello world!")
 
 
