df15ca15e0d3f234b565c74bcb6fc3b8f47f7ebb
Stepan Kudin <kudin.stepan@yandex.ru>
2019-09-22 00:45:04 +0300

    SVR wrapper init.

---
+++ atlasnet2/libs/svr_wrapper.py
@@ -0,0 +1,6 @@
+class SVRWrapper():
+    def __init__(self):
+        pass
+
+    def train(self):
+        pass
--- atlasnet2/scripts/train_svr.py
+++ atlasnet2/scripts/train_svr.py
@@ -1,11 +1,6 @@
-import logging
-import os
-
-import atlasnet2.configuration as conf
 import atlasnet2.libs.helpers as h
 from atlasnet2.libs.settings import Settings
-from atlasnet2.libs.visdom_wrapper import VisdomWrapper
-from atlasnet2.libs.network_wrapper import NetworkWrapper
+from atlasnet2.libs.svr_wrapper import SVRWrapper
 
 
 def main():
@@ -13,13 +8,7 @@ def main():
 
     experiment_path, snapshots_path, logger, vis = h.init_train(settings)
 
-    network = NetworkWrapper(mode="train", vis=vis, dataset_path=settings["dataset"], snapshots_path=snapshots_path,
-                             num_epochs=settings["num_epochs"], batch_size=settings["batch_size"],
-                             num_workers=settings["num_workers"], encoder_type=settings["encoder_type"],
-                             num_points=settings["num_points"], num_primitives=settings["num_primitives"],
-                             bottleneck_size=settings["bottleneck_size"], learning_rate=settings["learning_rate"],
-                             epoch_num_reset_optimizer=settings["epoch_num_reset_optimizer"],
-                             multiplier_learning_rate=settings["multiplier_learning_rate"])
+    network = SVRWrapper()
     network.train()
 
 
