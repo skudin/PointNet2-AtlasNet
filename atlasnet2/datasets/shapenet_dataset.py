c62a0e51cf3b21328fdfbb9e17f986729eac0903
Stepan Kudin <kudin.stepan@yandex.ru>
2019-09-24 20:51:48 +0300

    Dataset.

--- atlasnet2/datasets/shapenet_dataset.py
+++ atlasnet2/datasets/shapenet_dataset.py
@@ -1,12 +1,15 @@
 import logging
 import os
 import json
+import random
+from typing import Optional
 from collections import namedtuple
 
 import torch
 import torch.utils.data as data
 import torchvision.transforms as transforms
 import numpy as np
+from PIL import Image
 
 import atlasnet2.configuration as conf
 
@@ -16,13 +19,16 @@ logger = logging.getLogger(__name__)
 
 class ShapeNetDataset(data.Dataset):
     def __init__(self, svr: bool = False,
-                 dataset_path: str = os.path.join(conf.BASE_PATH, "data", "shapenet", "dataset"),
-                 mode: str = "train", num_points: int = 2500, include_normals: bool = False):
+                 dataset_path: str = os.path.join(conf.BASE_PATH, "data", "shapenet_tiny", "dataset"),
+                 mode: str = "train", num_points: int = 2500, include_normals: bool = False, gen_view: bool = False,
+                 fixed_render_num: Optional[int] = None):
         self._svr = svr
         self._dataset_path = dataset_path
         self._mode = mode
         self._num_points = num_points
         self._include_normals = include_normals
+        self._gen_vew = gen_view
+        self._fixed_render_num = fixed_render_num
 
         self._items = list()
 
@@ -45,10 +51,29 @@ class ShapeNetDataset(data.Dataset):
 
         point_cloud = raw_point_cloud[np.random.choice(raw_point_cloud.shape[0], self._num_points, replace=False), :]
         np.random.shuffle(point_cloud)
-
         point_cloud = torch.from_numpy(point_cloud)
 
-        return point_cloud, item.category, item.name
+        if self._svr:
+            if self._gen_vew:
+                render_path = item.renders[0]
+            elif self._fixed_render_num is not None:
+                render_path = item.renders[self._fixed_render_num]
+            else:
+                render_path = item.renders[random.randint(1, len(item.renders) - 1)]
+
+            image = Image.open(os.path.join(render_path))
+
+            if self._mode == "train":
+                image = self._data_augmentation(image) # random crop
+            else:
+                image = self._validating_transform(image) # center crop
+
+            image = self._finish_transforms(image) # scale and to tensor
+            image = image[: 3]
+
+            return image, point_cloud, item.category, item.name
+        else:
+            return point_cloud, item.category, item.name
 
     def _indexing(self):
         dataset_part_path = os.path.join(self._dataset_path, self._mode)
@@ -64,14 +89,14 @@ class ShapeNetDataset(data.Dataset):
                 category = json.load(fp)["category"]
 
             renders_path = os.path.join(case_path, "renders")
-            renders = ([file_obj for file_obj in os.listdir(renders_path) if
-                        os.path.isfile(os.path.join(renders_path, file_obj)) and file_obj.endswith(".png")])
+            renders = sorted([os.path.join(renders_path, file_obj) for file_obj in os.listdir(renders_path) if
+                              os.path.isfile(os.path.join(renders_path, file_obj)) and file_obj.endswith(".png")])
 
             self._items.append(
                 Item(item_name, category, os.path.join(dataset_part_path, item_name, "point_cloud.npy"), renders))
 
     def _init_transforms(self):
-        self._transforms = transforms.Compose([
+        self._finish_transforms = transforms.Compose([
             transforms.Resize(size=224, interpolation=2),
             transforms.ToTensor()
         ])
@@ -88,6 +113,6 @@ class ShapeNetDataset(data.Dataset):
 
 
 if __name__ == "__main__":
-    dataset = ShapeNetDataset()
+    dataset = ShapeNetDataset(svr=True)
     tmp = dataset[0]
     pass
