7cbaa3a69b85018452030b2c86feaa07ef5a5466
Stepan Kudin <kudin.stepan@yandex.ru>
2019-09-24 19:03:36 +0300

    Indexing renders.

--- atlasnet2/datasets/shapenet_dataset.py
+++ atlasnet2/datasets/shapenet_dataset.py
@@ -18,11 +18,11 @@ class ShapeNetDataset(data.Dataset):
     def __init__(self, svr: bool = False,
                  dataset_path: str = os.path.join(conf.BASE_PATH, "data", "shapenet", "dataset"),
                  mode: str = "train", num_points: int = 2500, include_normals: bool = False):
+        self._svr = svr
         self._dataset_path = dataset_path
         self._mode = mode
         self._num_points = num_points
         self._include_normals = include_normals
-        self._svr = svr
 
         self._items = list()
 
@@ -53,15 +53,22 @@ class ShapeNetDataset(data.Dataset):
     def _indexing(self):
         dataset_part_path = os.path.join(self._dataset_path, self._mode)
 
-        Item = namedtuple("Item", "name category point_cloud")
+        Item = namedtuple("Item", "name category point_cloud renders")
 
         items_list = sorted(os.listdir(dataset_part_path))
 
         for item_name in items_list:
-            with open(os.path.join(dataset_part_path, item_name, "meta.json"), "r") as fp:
+            case_path = os.path.join(dataset_part_path, item_name)
+
+            with open(os.path.join(case_path, "meta.json"), "r") as fp:
                 category = json.load(fp)["category"]
 
-            self._items.append(Item(item_name, category, os.path.join(dataset_part_path, item_name, "point_cloud.npy")))
+            renders_path = os.path.join(case_path, "renders")
+            renders = ([file_obj for file_obj in os.listdir(renders_path) if
+                        os.path.isfile(os.path.join(renders_path, file_obj)) and file_obj.endswith(".png")])
+
+            self._items.append(
+                Item(item_name, category, os.path.join(dataset_part_path, item_name, "point_cloud.npy", renders)))
 
     def _init_transforms(self):
         self._transforms = transforms.Compose([
