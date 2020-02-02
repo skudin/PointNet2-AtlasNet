f695cf44fea21283212a8adebd3b24666ccff118
Stepan Kudin <kudin.stepan@yandex.ru>
2020-02-03 00:28:02 +0300

    Normals calculation.

--- atlasnet2/scripts/meshing_point_cloud.py
+++ atlasnet2/scripts/meshing_point_cloud.py
@@ -1,4 +1,23 @@
+import numpy as np
+import open3d as o3d
+
+
 def main():
+    point_cloud_np = np.load("data/debug_meshing/1_primitive_2500_points.npy").squeeze()
+
+    pcd = o3d.geometry.PointCloud()
+    pcd.points = o3d.utility.Vector3dVector(point_cloud_np)
+
+    pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
+        radius=1.0, max_nn=30))
+    pcd.orient_normals_towards_camera_location(camera_location=[0.0, 0.0, 0.0])
+
+    normals = np.asarray(pcd.normals)
+    pcd.normals = o3d.utility.Vector3dVector(-normals)
+
+    mesh, _ = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(pcd)
+    o3d.io.write_triangle_mesh("data/debug_meshing/1_primitive_2500_points.ply", mesh)
+    o3d.io.write_point_cloud("data/debug_meshing/1_primitive_2500_points_point_cloud.ply", pcd)
     print("Hello world!")
 
 
