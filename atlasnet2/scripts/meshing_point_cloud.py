9b01423c2f2c1c522e01c7a69f9c69a28df682ba
Stepan Kudin <kudin.stepan@yandex.ru>
2020-02-09 17:13:12 +0300

    Default algo for finding normals.

--- atlasnet2/scripts/meshing_point_cloud.py
+++ atlasnet2/scripts/meshing_point_cloud.py
@@ -11,33 +11,15 @@ EPS = 1e-8
 STEP = 10.0
 
 
-def estimate_normals(point_cloud, radius=1.0, max_nn=30):
+def estimate_normals(point_cloud, radius=0.5, max_nn=30):
     point_cloud.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(
         radius=radius, max_nn=max_nn))
 
-    center = point_cloud.get_center()
+    point_cloud.orient_normals_towards_camera_location(camera_location=point_cloud.get_center())
 
-    dirs = (np.array((1.0, 0.0, 0.0), dtype=np.float64),
-            np.array((0.0, 1.0, 0.0), dtype=np.float64),
-            np.array((0.0, 0.0, 1.0), dtype=np.float64))
-    camera_locations = [center + sgn * STEP * dir for dir in dirs for sgn in (-1, 1)]
-    camera_locations.append(center)
-
-    normals_stat = defaultdict(Counter)
-    for camera_location in camera_locations:
-        point_cloud.orient_normals_towards_camera_location(camera_location=camera_location)
-
-        for i in range(len(point_cloud.points)):
-            normals_stat[i][tuple(point_cloud.normals[i])] += 1
-
-    points_with_problem_counter = 0
-    for key, normal_stat in normals_stat.items():
-        normal, counter = normal_stat.most_common(1)[0]
-        if counter < 7:
-            points_with_problem_counter += 1
-        point_cloud.normals[key] = np.array(normal, dtype=np.float64)
-
-    return points_with_problem_counter
+    normals = np.asarray(point_cloud.normals)
+    normals *= -1
+    point_cloud.normals = o3d.utility.Vector3dVector(normals)
 
 
 def main():
@@ -48,7 +30,7 @@ def main():
 
     o3d.io.write_point_cloud(OUTPUT_PREFIX + "_point_cloud.ply", pcd)
 
-    points_with_problem_counter = estimate_normals(pcd)
+    estimate_normals(pcd)
 
     o3d.io.write_point_cloud(OUTPUT_PREFIX + "_point_cloud_with_normals.ply", pcd)
 
