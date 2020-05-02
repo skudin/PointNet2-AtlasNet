286bf01ebcb1e19526bd026398ce434695efa5c6
Stepan Kudin <kudin.stepan@yandex.ru>
2020-05-02 20:01:27 +0300

    Common loop.

--- atlasnet2/scripts/metro.py
+++ atlasnet2/scripts/metro.py
@@ -1,4 +1,6 @@
 import argparse
+import os
+import os.path as osp
 
 
 def parse_command_prompt():
@@ -10,8 +12,52 @@ def parse_command_prompt():
     return parser.parse_args()
 
 
+def get_reference_filename(generated_filename, reference_path):
+    item_name, _ = osp.splitext(generated_filename)
+    reference_file_dir = osp.join(reference_path, item_name)
+
+    for file_obj in os.listdir(reference_file_dir):
+        file_obj_path = osp.join(reference_file_dir, file_obj)
+
+        if osp.isfile(file_obj_path) and file_obj.endswith(".ply"):
+            return file_obj_path
+
+
+def get_metro_distance():
+    # passed = False
+    # while not passed:
+    #     try:
+    #         ret = subprocess.run(
+    #             ["python3", "%s/atlasnet2/scripts/meshing_point_cloud.py" % conf.BASE_PATH, file_obj_path,
+    #              osp.join(output_dir, "%s.ply" % file_obj)], timeout=TIMEOUT)
+    #
+    #         if ret.returncode == 0:
+    #             passed = True
+    #         else:
+    #             print("Item was not passed. Retrying.")
+    #     except subprocess.TimeoutExpired:
+    #         print("Timeout. Retrying.")
+    pass
+
+
+def get_avg_metro_distance(generated_path, reference_path):
+    for file_obj in os.listdir(generated_path):
+        generated_filename = osp.join(generated_path, file_obj)
+
+        if not osp.isfile(generated_filename) or not file_obj.endswith(".ply"):
+            continue
+
+        reference_filename = get_reference_filename(file_obj, reference_path)
+
+        metro_distance = get_metro_distance()
+
+    pass
+
+
 def main():
     args = parse_command_prompt()
+
+    avg_metro_distance = get_avg_metro_distance(args.generated, args.reference)
 
     print("Hello world!")
 
