da2db48088e8f7895824edf4fb7fc86af91a084a
Stepan Kudin <kudin.stepan@yandex.ru>
2020-05-02 19:28:03 +0300

    Get args.

--- atlasnet2/scripts/metro.py
+++ atlasnet2/scripts/metro.py
@@ -1,4 +1,17 @@
+import argparse
+
+
+def parse_command_prompt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--reference", required=True, help="path to reference data")
+    parser.add_argument("--generated", required=True, help="path to generated data")
+
+    return parser.parse_args()
+
+
 def main():
+    args = parse_command_prompt()
+
     print("Hello world!")
 
 
