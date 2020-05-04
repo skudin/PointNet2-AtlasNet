bd8612450eae36800d1cb26c2072faf0bcc11cd2
Stepan Kudin <kudin.stepan@yandex.ru>
2020-05-05 01:22:04 +0300

    Refactoring.

--- atlasnet2/scripts/earth_mover_distance.py
+++ atlasnet2/scripts/earth_mover_distance.py
@@ -17,9 +17,8 @@ def parse_command_prompt():
     return parser.parse_args()
 
 
-def get_avg_emd(path):
-    avg_emd = 0.0
-    counter = 0
+def get_items(path):
+    items = list()
 
     for file_obj in os.listdir(path):
         file_obj_path = osp.join(path, file_obj)
@@ -27,25 +26,36 @@ def get_avg_emd(path):
         if not osp.isdir(file_obj_path):
             continue
 
+        items.append({
+            "name": file_obj,
+            "input": osp.join(file_obj_path, "input_point_cloud.ply"),
+            "output": osp.join(file_obj_path, "output_point_cloud.ply")
+        })
+
+    return items
+
+
+def get_avg_emd(path):
+    items = get_items(path)
+    avg_emd = 0.0
+
+    for num, item in enumerate(items, 1):
         start_calculation_time = time.time()
 
         input_point_cloud = torch.from_numpy(
-            np.asarray(o3d.io.read_point_cloud(osp.join(file_obj_path, "input_point_cloud.ply")).points)[
-                np.newaxis, ...]).cuda()
+            np.asarray(o3d.io.read_point_cloud(item["input"]).points)[np.newaxis, ...]).cuda()
         output_point_cloud = torch.from_numpy(
-            np.asarray(o3d.io.read_point_cloud(osp.join(file_obj_path, "output_point_cloud.ply")).points)[
-                np.newaxis, ...]).cuda()
+            np.asarray(o3d.io.read_point_cloud(item["output"]).points)[np.newaxis, ...]).cuda()
 
         metric_value = earth_mover_distance(input_point_cloud, output_point_cloud, transpose=False).item()
 
-        avg_emd += metric_value
-        counter += 1
+        avg_emd += metric_value / len(items)
 
         calculation_time = time.time() - start_calculation_time
+        print("Item %s is ready (%d/%d). EMD: %f. Calculation metric time: %f s" % (
+            item["name"], num, len(items), metric_value, calculation_time))
 
-        print("Item %s is ready. EMD: %f. Calculation metric time: %f s" % (file_obj, metric_value, calculation_time))
-
-    return avg_emd / counter
+    return avg_emd
 
 
 def main():
